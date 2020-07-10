119a0a50598328faea1c7a6e3991e371a71a3941
yongseondu <weed0124@naver.com>
2020-07-11 08:38:27 +0900

    DTO 추가

---
+++ src/main/java/com/yondu/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,25 @@
+package com.yondu.book.springboot.web.dto;
+
+import com.yondu.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
