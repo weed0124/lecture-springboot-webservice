8792f97265b5b1d040f15f177630b6c2fee33349
yongseondu <weed0124@naver.com>
2020-07-12 12:12:13 +0900

    누락 어노테이션 추가

--- src/main/java/com/yondu/book/springboot/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/yondu/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -2,7 +2,11 @@ package com.yondu.book.springboot.web.dto;
 
 import com.yondu.book.springboot.domain.posts.Posts;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 public class PostsSaveRequestDto {
     private String title;
     private String content;
