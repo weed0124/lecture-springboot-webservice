15fcca3ceb1fd0e7db9d64185c84aa36cf21b3e1
yongseondu <weed0124@naver.com>
2020-07-12 12:38:08 +0900

    수정 기능에 관련된 메소드 및 Dto 추가

--- src/main/java/com/yondu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/yondu/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.yondu.book.springboot.service.posts;
 
+import com.yondu.book.springboot.domain.posts.Posts;
 import com.yondu.book.springboot.domain.posts.PostsRepository;
+import com.yondu.book.springboot.web.dto.PostsResponseDto;
 import com.yondu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.yondu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,18 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
