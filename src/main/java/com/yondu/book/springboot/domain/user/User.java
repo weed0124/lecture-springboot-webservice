2fa85c134d07e53ab54e4b123b598632953a860c
yongseondu <weed0124@naver.com>
2020-07-17 07:14:46 +0900

    Role, User 및 Repository 추가

---
+++ src/main/java/com/yondu/book/springboot/domain/user/User.java
@@ -0,0 +1,44 @@
+package com.yondu.book.springboot.domain.user;
+
+import com.yondu.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+
+import javax.persistence.*;
+
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
